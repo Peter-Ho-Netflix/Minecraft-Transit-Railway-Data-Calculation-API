d0b19ab34d20829ce2b8281fdf5ea2136d86da28
Peter-Ho-Netflix <diannaobingdukb@aliyun.com>
2026-02-11 19:30:55 +0800

    Add UI components for distance calculation functionality
    - Expanded the user interface in `main.py` to include additional text controls and labels for height, distance, and slope length inputs.
    - Implemented a button for triggering the distance calculation, linking it to the `on_calculate_distance` method for processing user input.
    - Adjusted layout positions for better alignment of UI elements.

--- UI/main.py
+++ UI/main.py
@@ -15,16 +15,58 @@ class MainFrame(wx.Frame):
         self.Center()
 
         self.calculate_distance_label = wx.StaticText(self, label="计算在固定坡度要求下，升起对应高度所需的距离")
-        self.calculate_distance_label.SetFont(wx.Font(wx.FontInfo(12)))
+        font = self.calculate_distance_label.GetFont()
+        font.PointSize = 12
+        self.calculate_distance_label.SetFont(font)
         self.calculate_distance_label.SetPosition(wx.Point(10, 10))
 
         self.slope_text_ctrl = wx.TextCtrl(self)
-        self.slope_text_ctrl.SetPosition(wx.Point(10, 30))
+        self.slope_text_ctrl.SetPosition(wx.Point(10, 35))
         self.slope_text_ctrl.SetSize(wx.Size(100, 20))
         self.slope_text_ctrl.SetValue("0.0")
         self.slope_text_unit = wx.StaticText(self, label="‰")
-        self.slope_text_unit.SetPosition(wx.Point(110, 30))
-        self.slope_text_unit.SetSize(wx.Size(20, 20))
+        self.slope_text_unit.SetPosition(wx.Point(110, 35))
+
+        self.height_text_ctrl = wx.TextCtrl(self)
+        self.height_text_ctrl.SetPosition(wx.Point(210, 35))
+        self.height_text_ctrl.SetSize(wx.Size(100, 20))
+        self.height_text_ctrl.SetValue("0.0")
+        self.height_text_unit = wx.StaticText(self, label="米")
+        self.height_text_unit.SetPosition(wx.Point(310, 35))
+
+        self.distance_text_ctrl = wx.TextCtrl(self)
+        self.distance_text_ctrl.SetPosition(wx.Point(10, 70))
+        self.distance_text_ctrl.SetSize(wx.Size(100, 20))
+        self.distance_text_ctrl.SetValue("0.0")
+        self.distance_text_unit = wx.StaticText(self, label="米")
+        self.distance_text_unit.SetPosition(wx.Point(110, 70))
+
+        self.distance_rounded_text_ctrl = wx.TextCtrl(self)
+        self.distance_rounded_text_ctrl.SetPosition(wx.Point(210, 70))
+        self.distance_rounded_text_ctrl.SetSize(wx.Size(100, 20))
+        self.distance_rounded_text_ctrl.SetValue("0")
+        self.distance_rounded_text_unit = wx.StaticText(self, label="米")
+        self.distance_rounded_text_unit.SetPosition(wx.Point(310, 70))
+
+        self.slope_length_text_ctrl = wx.TextCtrl(self)
+        self.slope_length_text_ctrl.SetPosition(wx.Point(10, 95))
+        self.slope_length_text_ctrl.SetSize(wx.Size(100, 20))
+        self.slope_length_text_ctrl.SetValue("0.0")
+        self.slope_length_text_unit = wx.StaticText(self, label="米")
+        self.slope_length_text_unit.SetPosition(wx.Point(110, 95))
+
+        self.calculate_distance_button = wx.Button(self, label="计算")
+        self.calculate_distance_button.SetPosition(wx.Point(10, 120))
+        self.calculate_distance_button.SetSize(wx.Size(100, 20))
+        self.calculate_distance_button.Bind(wx.EVT_BUTTON, self.on_calculate_distance)
+
+    def on_calculate_distance(self, event):
+        slope = float(self.slope_text_ctrl.GetValue())/1000
+        height = float(self.height_text_ctrl.GetValue())
+        result = calc_distance(slope, height)
+        self.distance_text_ctrl.SetValue(str(result["distance"]))
+        self.distance_rounded_text_ctrl.SetValue(str(result["distance_rounded"]))
+        self.slope_length_text_ctrl.SetValue(str(result["slope_length"][0]))
 
 class App(wx.App):
     def OnInit(self):
